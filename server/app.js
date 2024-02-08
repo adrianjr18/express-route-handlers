ab566d8ad3102c80e52a464d835faca3164e97c1
Adrian <adrian.sanchezjr18@github.com>
2024-02-07 18:01:40 -0800

    phase 1

--- server/app.js
+++ server/app.js
@@ -22,9 +22,62 @@ const {
 } = require('./data');
 
 const express = require('express');
-const app = express();
 
-// Your code here 
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+// Your code here
+app.get('/artists/:artistId', (req, res) => {
+  // Your data to be sent in the JSON response
+  const { artistId } = req.params;
+  const artist = getArtistByArtistId(artistId);
+
+  // Sending JSON response
+  res.status(200).json(artist);
+
+});
+
+app.get('/artists', (req, res) => {
+  // Your data to be sent in the JSON response
+
+  const artists = getAllArtists();
+
+  // Sending JSON response
+  res.status(200).json(artists);
+
+});
+
+app.get('/artists/latest', (req, res) => {
+  // Your data to be sent in the JSON response
+
+  const latestartist = getLatestArtist();
+
+  // Sending JSON response
+  res.status(200).json(latestartist);
+
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  // Your data to be sent in the JSON response
+
+  const latestAlbum = getAlbumsForLatestArtist();
+
+  // Sending JSON response
+  res.status(200).json(latestAlbum);
+
+});
+
+app.post('/api/users', function(req, res) {
+  const { name } = req.body;
+  const newArtist = addArtist(name)
+  console.log(newArtist)
+  res.send({
+    'user_id': newArtist.artistId,
+    'name': name
+
+  });
+});
 
 // DO NOT MODIFY
 if (require.main === module) {
